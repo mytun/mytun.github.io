cdf2a391fc8203050d5c7ee55432768902ee4f26
mytun <lxyg06@163.com>
2021-01-03 20:26:26 +0800

    Site updated: 2021-01-03 20:26:26

--- js/js.js
+++ js/js.js
@@ -1,26 +1,21 @@
-
-
-function setCookie(key, value) {
-    localStorage.setItem(key, value);
-}
-
-function getCookie(key) {
-    var data = localStorage.getItem(key);
-    return data
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+});
 
 const date = new Date()
 const hours = date.getHours()
 $("#date").text(date.getFullYear())
-// if(hours<18&&hours>6){
-//     $("#footer").attr("style", "color: #51525d;");
-//     $(".flink").attr("style", "color: #51525d;");
-//     $(".ba").attr("style", "color: #51525d;");
-//     $("#bodyx").attr("class", "bg_while");
-// } else {
-//     $("#footer").attr("style", "");
-//     $(".flink").attr("style", "");
-//     $("#bodyx").attr("class", "");
-//     $(".ba").attr("style", "");
-// }
+if(hours<18&&hours>6){
+    $("#footer").attr("style", "color: #51525d;");
+    $(".flink").attr("style", "color: #51525d;");
+    $(".ba").attr("style", "color: #51525d;");
+    $("#bodyx").attr("class", "bg_while");
+} else {
+    $("#footer").attr("style", "");
+    $(".flink").attr("style", "");
+    $("#bodyx").attr("class", "");
+    $(".ba").attr("style", "");
+}
 
